1d94ac0ce162240483be4af5544fa0dc516784a6
sofiia1998 <Sofiia_Kalishchuk@epam.com>
2022-10-16 21:08:57 +0200

    second and third hometasks

---
+++ src/com/epam/courses/principles/Student.java
@@ -0,0 +1,44 @@
+package com.epam.courses.principles;
+
+
+public abstract class Student implements AgeOfTheStudent{
+    private String name;
+    boolean bigFamily;
+
+    final String country = "Ukraine";
+    private int age;
+
+    public void studik() {
+        System.out.println("This is a new student:");
+    }
+
+    public void printStudik() {
+        studik();
+        System.out.println(getName());
+    }
+
+    public int getAge() {return age;}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        if (age >= 18) {
+            this.age = age;
+        } else {
+            System.out.println("Invalid age");
+        }
+    }
+    public void setName(String name) {
+            this.name = name;
+    }
+    public Student() {System.out.println("Student(w/o parameters)");}
+
+    public Student(boolean bigFamily) {
+        //System.out.println("Student(with parameters)");
+        this.bigFamily = bigFamily;
+    }
+
+
+}
---
+++ src/com/epam/courses/principles/StudentGroup.java
@@ -0,0 +1,17 @@
+package com.epam.courses.principles;
+
+
+public class StudentGroup extends StudentFaculty {
+    String group;
+
+    StudentGroup(boolean bigFamily, String faculty, String group) {
+        super(bigFamily, faculty);
+        this.group = group;
+    }
+
+    StudentGroup() {System.out.println("Student(w/o parameters)");}
+
+    public void totalInformation(String name, String faculty, String group) {
+        System.out.format("To sum up, %s is from %s faculty and %s group", name, faculty, group);
+    }
+}
